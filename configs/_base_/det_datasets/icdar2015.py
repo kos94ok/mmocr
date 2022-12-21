a939b823704377001c9a83cbb18b42c9ab8f17e6
Merkurev Maxim <96414688+kos94ok@users.noreply.github.com>
2022-12-21 17:26:52 +0500

    Update icdar2015.py

--- configs/_base_/det_datasets/icdar2015.py
+++ configs/_base_/det_datasets/icdar2015.py
@@ -3,14 +3,14 @@ data_root = 'data/icdar2015'
 
 train = dict(
     type=dataset_type,
-    ann_file=f'{data_root}/instances_training.json',
-    img_prefix=f'{data_root}/imgs',
+    ann_file=f'{data_root}/instances_training-2.json',
+    img_prefix=f'{data_root}/train',
     pipeline=None)
 
 test = dict(
     type=dataset_type,
-    ann_file=f'{data_root}/instances_test.json',
-    img_prefix=f'{data_root}/imgs',
+    ann_file=f'{data_root}/instances_validation.json',
+    img_prefix=f'{data_root}/validation',
     pipeline=None)
 
 train_list = [train]
