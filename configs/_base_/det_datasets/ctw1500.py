9aa86495b1a851137047d872eeace09463e9c1c3
Merkurev Maxim <96414688+kos94ok@users.noreply.github.com>
2023-03-19 13:46:15 +0500

    Update ctw1500.py

--- configs/_base_/det_datasets/ctw1500.py
+++ configs/_base_/det_datasets/ctw1500.py
@@ -1,16 +1,16 @@
 dataset_type = 'IcdarDataset'
-data_root = 'data/ctw1500'
+data_root = 'data/icdar2015'
 
 train = dict(
     type=dataset_type,
     ann_file=f'{data_root}/instances_training.json',
-    img_prefix=f'{data_root}/imgs',
+    img_prefix=f'{data_root}/train',
     pipeline=None)
 
 test = dict(
     type=dataset_type,
-    ann_file=f'{data_root}/instances_test.json',
-    img_prefix=f'{data_root}/imgs',
+    ann_file=f'{data_root}/instances_validation.json',
+    img_prefix=f'{data_root}/validation',
     pipeline=None)
 
 train_list = [train]
